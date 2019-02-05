4386c4936c139154094e4daba4a45707899d9573
Kayleigh Roberts <kayleigh.ruth.roberts@gmail.com>
2019-02-05 21:05:40 +0000

    first push, use gifs only, no webm

--- flask_app.py
+++ flask_app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/spaceport', methods=['GET'])
+def mainspace():
+    return render_template("error.html")
